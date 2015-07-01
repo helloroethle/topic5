e8f5a4571e4406a0f40332a8fc0bc22d37db94df
Justin Roethle <justin.roethle@gmail.com>
2015-07-01 15:28:09 -0500

    wysiwyg editor and article import working for create article

--- client/templates/articles/js/create_article.js
+++ client/templates/articles/js/create_article.js
@@ -1,4 +1,43 @@
 Template.createArticle.rendered = function(){
   $('.tags-input').tagsinput();
+  this.editor = new MediumEditor('.editable', { 
+    toolbar: {
+        buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote', 'orderedlist', 'unorderedlist']
+    },
+    buttonLabels: 'fontawesome' 
+  });
 }
 
+Template.createArticle.events({
+  'click .add-link' : function(e){
+    $('#linkModal').modal();
+  },
+  'click .import-article': function (e, template) {
+    $('#linkModal').modal('hide');
+    var link = template.find('[name=addlink]').value;
+    Meteor.call("getReadabilityArticle", link, function(error, results) {
+      results = JSON.parse(results.content);
+      console.log(results);
+      template.find('[name=url]').value = results.url;
+      template.find('[name=source]').value = results.domain;
+      template.find('[name=title]').value = results.title;
+      template.find('[name=author]').value = results.author;
+      template.find('[name=published]').value = results.date_published;
+      template.find('[name=summary]').value = results.excerpt;
+      template.editor.destroy();
+      template.find('[name=html]').value = results.content;
+      template.editor.setup();
+      // $('div.editable').text(results.content);
+    });
+    // Meteor.call('getGraityArticle', link, function(error, results){
+    //   template.find('[name=url]').value = results.canonicalLink;
+    //   // template.find('[name=source]').val(results.url);
+    //   template.find('[name=title]').value = results.title;
+    //   // template.find('[name=author]').val(results.url);
+    //   // template.find('[name=published]').val(results.url);
+    //   // template.find('[name=summary]').val(results.url);
+    //   template.find('.editable').value = results.text;
+    //   template.find('[name=html]').value = results.text;
+    // });
+  }
+});
--- client/templates/articles/js/list_article.js
+++ client/templates/articles/js/list_article.js
@@ -7,16 +7,3 @@ Template.listArticle.helpers({
     return Articles.find({}, queryOptions);
   }
 });
-
-Template.listArticle.events({
-  'click .importArticle': function (e) {
-    var link = 'http://money.cnn.com/2015/07/01/news/economy/greece-bailout-concessions/index.html';
-    Meteor.call("getReadabilityArticle", link, function(error, results) {
-        console.log(results.content); //results.data should be a JSON object
-    });
-    Meteor.call('getGraityArticle', link, function(error, results){
-        console.log(results);
-    });
-
-  }
-});
