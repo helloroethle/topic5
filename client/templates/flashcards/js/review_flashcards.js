53c700e59037b3fc2b3acb80bfcafb99c2d23599
Justin Roethle <justin.roethle@gmail.com>
2015-08-21 16:47:21 -0500

    flashcard review page working with keyboard shortcuts

--- client/templates/flashcards/js/review_flashcards.js
+++ client/templates/flashcards/js/review_flashcards.js
@@ -2,9 +2,17 @@ window.flashcardOrder = [];
 
 Template.reviewFlashcards.events({
   'click .card': function (e, template) {
-    $(e.currentTarget).toggleClass('flipped');
+    flip();
+  },
+  'click .arrow-right':function (e, template){
+    right();
+  },
+  'click .arrow-left': function (e, template){
+    left();
   }
 });
+
+
 
 Template.reviewFlashcards.created = function () {
   Session.set('current_question_index', 0);
@@ -22,26 +30,48 @@ Template.reviewFlashcards.created = function () {
   
 };
 
+var setFlash = function(selector){
+  var $flash = $(selector).find('span.badge').addClass('flash');
+  setTimeout(function(){ $flash.removeClass('flash'); }, 200);
+}
+
+var left = function(){
+  var index = Session.get('current_question_index');
+  if(index > 0){
+    index -= 1;
+    Session.set('current_question_index', index);
+  }
+}
+
+var right = function(){
+ var index = Session.get('current_question_index');
+ var totalQuestions = Session.get('total_questions');
+  if(index + 1 < totalQuestions){
+    index += 1;
+    Session.set('current_question_index', index);
+  }
+}
+
+var flip = function(){
+  $('.card').toggleClass('flipped');
+}
+
 Template.reviewFlashcards.rendered = function () {
   $("body").keydown(function(e) {
-    if(e.keyCode == 32) { // spacebar
+    if(e.keyCode == 32) { // spacebar - flip
       e.preventDefault();
-      $('.card').toggleClass('flipped');
+      setFlash('.spacebar-shortcut');
+      flip();
     }
-    else if(e.keyCode == 37) { // left
-      var index = Session.get('current_question_index');
-      if(index > 0){
-        index -= 1;
-        Session.set('current_question_index', index);
-      }
+    else if(e.keyCode == 37) { // left - previous
+      e.preventDefault();
+      setFlash('.left-shortcut');
+      left();
     }
-    else if(e.keyCode == 39) { // right
-     var index = Session.get('current_question_index');
-     var totalQuestions = Session.get('total_questions');
-      if(index + 1 < totalQuestions){
-        index += 1;
-        Session.set('current_question_index', index);
-      }
+    else if(e.keyCode == 39) { // right - next
+      e.preventDefault();
+      setFlash('.right-shortcut');
+      right();
     }
   }); 
 };
--- client/templates/flashcards/js/review_flashcards_sidebar.js
+++ client/templates/flashcards/js/review_flashcards_sidebar.js
@@ -1,33 +1,51 @@
-Template.reviewFlashcardsSidebar.helpers({
-  questionsCorrect: function () {
-    return Session.get('current_questions_correct');
-  },
-  questionsIncorrect: function (){
-    return Session.get('current_questions_incorrect');
-  },
-  questionsRemaining: function (){
-    return Session.get('current_questions_remaining');
-  },
-});
+Template.reviewFlashcardsSidebar.destroyed = function () {
+  $("body").off('keydown');
+};
 
-Template.reviewFlashcardsSidebar.events({
-  'click .restart':function (e, template){
-    Session.set('start_time', moment().toString());
-    Session.set('current_question_index', 0);
-    window.flashcardOrder = _.range(0, Session.get('total_questions'));
-    Session.set('shuffle_count', Session.get('shuffle_count') + 1);
-  },
-  'click .shuffle':function(e, template){
-    Session.set('shuffle_count', Session.get('shuffle_count') + 1);
-    window.flashcardOrder = _.shuffle(window.flashcardOrder); 
-    Session.set('start_time', moment().toString());
-    Session.set('current_question_index', 0);    
-  },
-  'click .take-quiz':function(e, template){
-    Router.go('takeQuiz', {'_id' : template.data._id});
-  },
-  'click .auto-play':function(e, template){
-    console.log('hey oh');
+Template.reviewFlashcardsSidebar.rendered = function () {
+ $("body").keydown(function(e) {
+    if(e.keyCode == 80){ // p - auto play
+      e.preventDefault();
+      setFlash('.p-shortcut');
+      autoplay();
+    }
+    else if(e.keyCode == 82){ // r - restart
+      e.preventDefault();
+      setFlash('.r-shortcut');
+      restart();
+    }
+    else if(e.keyCode == 83){ // s - shuffle
+      e.preventDefault();
+      setFlash('.s-shortcut');
+      shuffle();
+    }
+    // else if(e.keyCode == 84){ // t - take quiz
+    //   e.preventDefault();
+    //   Router.go('takeQuiz', {'_id' : this._id});
+    // } 
+  }); 
+};
+
+var setFlash = function(selector){
+  var $flash = $(selector).find('span.badge').addClass('flash');
+  setTimeout(function(){ $flash.removeClass('flash'); }, 200);
+}
+
+var restart = function(){
+  Session.set('start_time', moment().toString());
+  Session.set('current_question_index', 0);
+  window.flashcardOrder = _.range(0, Session.get('total_questions'));
+  Session.set('shuffle_count', Session.get('shuffle_count') + 1);
+}
+
+var shuffle = function(){
+  Session.set('shuffle_count', Session.get('shuffle_count') + 1);
+  window.flashcardOrder = _.shuffle(window.flashcardOrder); 
+  Session.set('start_time', moment().toString());
+  Session.set('current_question_index', 0);    
+}
+
+var autoplay = function(){
     if(Session.get('autoPlayIntervalId') != 0){
       // stop autoplay
       clearTimeout ( Session.get('autoPlayIntervalId' ) );
@@ -42,22 +60,35 @@ Template.reviewFlashcardsSidebar.events({
           if(state == 'flip'){
             $('.card').toggleClass('flipped');
             Session.set('auto_play_state', 'next');
-            console.log('flip called');
           }
           else if(state == 'next'){
             $('.card').toggleClass('flipped');
-             var index = Session.get('current_question_index');
-             var totalQuestions = Session.get('total_questions');
-              if(index + 1 < totalQuestions){
-                index += 1;
-                Session.set('current_question_index', index);
-              }
-            Session.set('auto_play_state', 'flip');
-            console.log('next called')
+            setTimeout(function(){  
+               var index = Session.get('current_question_index');
+               var totalQuestions = Session.get('total_questions');
+                if(index + 1 < totalQuestions){
+                  index += 1;
+                  Session.set('current_question_index', index);
+                }
+              Session.set('auto_play_state', 'flip'); 
+            }, 350);
             }
         }, 3500 );
       Session.set('autoPlayIntervalId', autoPlayIntervalId);
     }
+}
 
+Template.reviewFlashcardsSidebar.events({
+  'click .restart':function (e, template){
+    restart();
+  },
+  'click .shuffle':function(e, template){
+    shuffle();
+  },
+  'click .take-quiz':function(e, template){
+    Router.go('takeQuiz', {'_id' : template.data._id});
+  },
+  'click .auto-play':function(e, template){
+    autoplay();
   },
 });
